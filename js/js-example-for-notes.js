3c2fbd7a854f141bb4d90d55726e7af04e3aaa04
Michaela <michaela.arteberry@gmail.com>
2023-10-13 13:02:55 -0400

    js w/ html notes

---
+++ js/js-example-for-notes.js
@@ -0,0 +1,9 @@
+"use strict";
+let favColor= "purple"
+let myName ="Michaela"
+// console.log prints to the console//
+alert("Hello from JavaScript");
+// we can concantenate inside of an alert //
+alert("Hello from " + myName)
+
+// confirmed
