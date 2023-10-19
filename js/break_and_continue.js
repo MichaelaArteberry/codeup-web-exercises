aaecd84081bf697324861519bf62cd96279690ea
Michaela <michaela.arteberry@gmail.com>
2023-10-19 14:59:36 -0400

    This is the CORRECT BREAK and CONTINUE.

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -1,12 +1,25 @@
-function oddNumbers() {
-    let userInput = prompt("Please enter an ODD number between 1 and 50.");
-    for (let i = 0; i = 51; i++) {
-        if(i % 2 !== 0) {
-            console.log(i)
-        }
-        if (i === userInput) {
-            console.log("Looks like we are skipping this one!");
-            break;
-        } continue;
+"use strict"
+
+
+let userInput = parseFloat(prompt("Please enter an ODD number between 1 and 50."));
+let numbers = (userInput < 50 && userInput > 0 && (userInput % 2) !== 0);
+
+while(numbers === false) {
+    userInput = prompt("Invalid number! Number must be odd and between 1 and 50!");
+    numbers = (userInput < 50 && userInput > 0 && (userInput % 2) !== 0);
+    if (numbers === true) {
+        break;
+    }
+}
+console.log("We are skipping " + userInput);
+
+for (let i = 1; i < 50; i++){
+    if (i % 2 === 0) {
+        continue;
+    }
+    if (userInput === i) {
+        console.log("Looks like we are skipping " + i);
+    } else{
+        console.log("Odd Number is " + i);
     }
 }
